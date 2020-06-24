66b744e61c5492950e6f036aab51046abd920877
JT Park <bajutae@gmail.com>
2020-06-24 14:09:55 +0900

    modified var

--- aks-cluster.tf
+++ aks-cluster.tf
@@ -2,6 +2,11 @@ resource "random_pet" "prefix" {}
 
 provider "azurerm" {
   version = "~> 1.27.0"
+
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 }
 
 resource "azurerm_kubernetes_cluster" "default" {
--- variables.tf
+++ variables.tf
@@ -17,3 +17,20 @@ variable "azurerm_resource_group_name" {
     type    = string
     default = "rgrgrgrg"
 }
+
+variable "subscription_id" {
+    type    = string
+    default = "00000000-0000-0000-0000-000000000000"
+}
+variable "client_id" {
+    type    = string
+    default = "00000000-0000-0000-0000-000000000000"
+}
+variable "client_secret" {
+    type    = string
+    default = "00000000-0000-0000-0000-000000000000"
+}
+variable "tenant_id" {
+    type    = string
+    default = "00000000-0000-0000-0000-000000000000"
+}
