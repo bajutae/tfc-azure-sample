edeaf2531389da60c1a33031e060ccdb49fce8f8
JT Park <bajutae@gmail.com>
2020-06-24 14:02:15 +0900

    output modified

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,3 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.name
-}
-
 output "kubernetes_cluster_name" {
-  value = azurerm_kubernetes_cluster.name
+  value = azurerm_kubernetes_cluster.default.name
 }
